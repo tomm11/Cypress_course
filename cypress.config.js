92206ec72cc3875a1bdb6bb0c935ac7cb1e009cb
Tomas <miliatomas@gmail.com>
2023-07-13 12:21:16 +0200

    Subiendo

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+
+    // mas info en https://docs.cypress.io/api/commands/viewport
+    viewportWidth: 1500,
+    viewportHeight: 900,
+    chromeWebSecurity: false,
+    pageLoadTimeout: 9000,
+    defaultCommandTimeout:11000,
+
+    
+    setupNodeEvents(on, config) {
+      // implement node event listeners here      
+     
+      
+    },
+  },
+});
